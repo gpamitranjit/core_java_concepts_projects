e649b20f6f3198d9f350fd7294a0176c20a04de6
Amit Patil <gpamitranjit@gmail.com>
2018-02-20 20:29:42 +0530

    Method return type can't be different if so then the return type should be covariant

---
+++ core_java_concepts_projects/src/com/amdocs/MethodOverridingTest.java
@@ -0,0 +1,23 @@
+package com.amdocs;
+
+class Parent {
+	protected int getNumbericValue() {
+		System.out.println("Message from Parent class");
+		return 1;
+	}
+}
+
+public class MethodOverridingTest extends Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	protected long getNumbericValue() {
+		System.out.println("Message from MethodOverridingTest class");
+		return 2L;
+	}
+
+}
