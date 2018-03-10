4519709e318fcb606c487fd89cec61451f46d4e8
Amit Patil <gpamitranjit@gmail.com>
2018-03-10 19:22:50 +0530

    This java program illustrate the advantage of accessing the data members only using setters and getters methods

---
+++ core_java_concepts_projects/src/com/amdocs/datamember/polymorphism/DataMemberPolymorphism.java
@@ -0,0 +1,42 @@
+package com.amdocs.datamember.polymorphism;
+
+class Bike {
+	int speedLimit = 100;
+
+	protected int getSpeedLimit() {
+		System.out.println("called getSpeedLimit() method of Bike class");
+		return speedLimit;
+	}
+
+	protected void setSpeedLimit(int speedLimit) {
+		this.speedLimit = speedLimit;
+	}
+	
+}
+
+class Splender extends Bike {
+	int speedLimit = 60;
+
+	protected int getSpeedLimit() {
+		System.out.println("called getSpeedLimit() method of Splender class");
+		return this.speedLimit;
+	}
+
+	protected void setSpeedLimit(int speedLimit) {
+		this.speedLimit = speedLimit;
+	}
+	
+	
+	
+}
+
+public class DataMemberPolymorphism {
+
+	public static void main(String[] args) {
+		Bike bike = new Splender();
+		
+		System.out.println("speedLimit: "  + bike.getSpeedLimit());
+		
+	}
+
+}
