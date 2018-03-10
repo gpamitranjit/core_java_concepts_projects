d0a73366215ce7fb190aa800cb4c3b151f90abba
Amit Patil <gpamitranjit@gmail.com>
2018-03-10 18:45:09 +0530

    This java program illustrate the use of static method overloading in java and indeed it does happen in java

---
+++ core_java_concepts_projects/src/com/amdocs/OverloadStaticMethod.java
@@ -0,0 +1,19 @@
+package com.amdocs;
+
+public class OverloadStaticMethod {
+
+	public static void main(String[] args) {
+//		showcasing the compile time polymorphism in java
+		display();
+		display(100);
+	}
+
+	
+	static void display() {
+		System.out.println("display() method called!");
+	}
+	
+	static void display(int a) {
+		System.out.println("display(int) method called with parameter: " + a);
+	}
+}
