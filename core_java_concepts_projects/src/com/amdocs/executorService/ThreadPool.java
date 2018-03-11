e7786686a9cf43743371ba162bc8b1236d7164eb
Amit Patil <gpamitranjit@gmail.com>
2018-03-11 22:16:45 +0530

    Using ExecutorService to create the thread pool of fixed size and hence increase the performance of application by avoiding the need to create new thread and reusing the thread from pool

---
+++ core_java_concepts_projects/src/com/amdocs/executorService/ThreadPool.java
@@ -0,0 +1,33 @@
+package com.amdocs.executorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Using ExecutorService to create the thread pool of fixed size and hence increase the performance of application by avoiding the need to create new thread and reusing the thread from pool
+ * 
+ * @author Amit
+ *
+ */
+public class ThreadPool {
+
+	public static void main(String[] args) {
+		
+//			creating the thread pool of 5 threads
+			ExecutorService executorService = Executors.newFixedThreadPool(5);
+			
+			for(int i=0; i < 10; i++) { 
+				
+				Runnable runnableTask = new WorkerThread("" + i);
+				executorService.execute(runnableTask);
+				
+			}
+			
+			executorService.shutdown();
+			
+			while(!executorService.isTerminated()) {}
+			
+			System.out.println("Finished all threads");
+	}
+
+}
---
+++ core_java_concepts_projects/src/com/amdocs/executorService/WorkerThread.java
@@ -0,0 +1,40 @@
+package com.amdocs.executorService;
+
+public class WorkerThread implements Runnable {
+
+	private String message;
+	
+	protected String getMessage() {
+		return message;
+	}
+
+	protected void setMessage(String message) {
+		this.message = message;
+	}
+
+	public WorkerThread(String message) {
+		this.message = message;
+	}
+	
+	@Override
+	public void run() {
+		
+		System.out.println(Thread.currentThread().getName() + " (start) message: " + this.getMessage());
+		processMessage();
+		System.out.println(Thread.currentThread().getName() +  " (end)");
+		
+	}
+	
+	private void processMessage() {
+		try {
+			
+			Thread.sleep(500);
+			
+		} catch (InterruptedException e) {
+			
+			System.out.println("InterruptedException: " + e.getMessage());
+			
+		}
+	}
+
+}
