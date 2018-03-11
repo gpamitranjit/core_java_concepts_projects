c07cbc6508078f334236673acc5faeeac9bde249
Amit Patil <gpamitranjit@gmail.com>
2018-03-11 10:48:04 +0530

    an interface containing nested class

---
+++ core_java_concepts_projects/src/com/amdocs/nestedinterface/NestedClassInInterface.java
@@ -0,0 +1,13 @@
+package com.amdocs.nestedinterface;
+
+/**
+ * We can define a class inside interface in such cases java compiler creates static nested class!
+ * 
+ * @author Amit
+ *
+ */
+public interface NestedClassInInterface {
+	class NestedClass {
+		
+	}
+}
