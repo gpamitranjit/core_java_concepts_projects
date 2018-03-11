64f29193f761d2e6a4a5afd58fa2c98656cfc13a
Amit Patil <gpamitranjit@gmail.com>
2018-03-11 17:40:39 +0530

    Two Thread working on same thread and we have avoided the race condition by using synchronized block and hence until the firstly entered thread completes' it's work the second thread won't be able to enter the block and once the firstly entered thread completes it's task the second thread enters and get it's job done

--- core_java_concepts_projects/src/com/amdocs/multithreading/UsingRunnableInterface.java
+++ core_java_concepts_projects/src/com/amdocs/multithreading/UsingRunnableInterface.java
@@ -1,11 +1,32 @@
 package com.amdocs.multithreading;
 
-
+/*
+Two Thread working on same thread and we have avoided the race condition by using synchronized block 
+and hence until the firstly entered thread completes' it's work the second thread won't be able to enter the block
+and once the firstly entered thread completes it's task the second thread enters and get it's job done
+*/
 class MyThread1 implements Runnable {
 
 	@Override
 	public void run() {
-		System.out.println("my Thread is running");
+		
+		synchronized(this) {
+			
+			for(int i=1; i <= 5; i++) {
+				
+				try {
+
+					Thread.sleep(500);
+
+				} catch (InterruptedException e) {
+					
+					System.err.println(e.getMessage());
+					
+				}
+				
+				System.out.println(i);
+			}
+		}
 	}
 	
 }
@@ -19,8 +40,12 @@ public class UsingRunnableInterface {
 	public static void main(String[] args) {
 		
 		MyThread1 thread = new MyThread1();
+		
 		Thread actualThreadObject = new Thread(thread);
+		Thread actualThreadObject2 = new Thread(thread);
+		
 		actualThreadObject.start();
+		actualThreadObject2.start();
 	}
 
 }
