412a1d988200d2ed80d5a4fba68103c665dc10f5
Amit Patil (Krishna) <gpamitranjit@gmail.com>
2018-04-05 18:18:42 +0530

    demonstration of regex in java

---
+++ core_java_concepts_projects/src/com/amdocs/regex/RegEx.java
@@ -0,0 +1,51 @@
+package com.amdocs.regex;
+
+import java.util.regex.Pattern;
+
+public class RegEx {
+
+	public static void main(String[] args) {
+		String pattern = "[amn]@\\.com";	//a or m or n and @.com for pattern
+		boolean result = Pattern.matches(pattern, "a@.com");
+		
+		System.out.println("result: " + result);
+		
+		
+		String pattern2 = "a?";	//once or not at all
+		boolean patternResult2 = Pattern.matches(pattern2, "a");
+		System.out.println("booleanResult2: " + patternResult2);
+		
+		
+		String pattern3 = "a+";	//one or more occourences
+		boolean patternResult3 = Pattern.matches(pattern3, "aaaaaaaaaaaaaaaa");
+		System.out.println("patternrResult3: " + patternResult3);
+		
+		String pattern4 = "a{2}[a-z]*";
+		boolean patternResult4 = Pattern.matches(pattern4, "aaa");
+		System.out.println("patternResult4: " + patternResult4);
+		
+		
+		String pattern5 = "a{2,}";	// a should occur 2 OR more times.
+		boolean patternResult5 = Pattern.matches(pattern5, "aaa");
+		System.out.println("patternResult5: " + patternResult5);
+		
+		
+		String pattern6 = "a{4,8}";	// a should occur between 4 to 8 times (both inclusive)
+		boolean patternResult6 =  Pattern.matches(pattern6, "aaaaaaaaa");
+		System.out.println("patternResult6: " + patternResult6);
+		
+		//using ? Quantifier
+		String pattern7 = "[amn]?";	//once OR zero times a OR m OR n character 
+		boolean patternResult7 = Pattern.matches(pattern7, "a");
+		System.out.println("patternResult7: " + patternResult7);
+		
+		
+		//using  + Quantifier
+		String pattern8 = "[amn]+";
+		boolean patternResult8 = Pattern.matches(pattern8, "aaaaaaaammmmmmmmmmmmmmmm");
+		System.out.println("patternResult8: " + patternResult8);
+		
+		String pattern9 = "";
+	}
+
+}
